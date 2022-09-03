794ad1ad75ffd62751887b018e47e5057b625740
filunieto <fsevein@gmail.com>
2022-09-03 13:33:56 +0200

    primeros pasos con la libreria. ft printf y libft anadidos

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fractol
 Fractol proyect 42
+
+https://github.com/codam-coding-college/MLX42#download-and-build---mlx42
+
+git clone https://github.com/codam-coding-college/MLX42.git
+
+brew update
+brew install glfw
+
+https://github.com/codam-coding-college/MLX42#download-and-build---mlx42
+
+cd mlx42
+make
+
+gcc main.c libmlx42.a -lglfw -L /Users/fnieves-/.brew/Cellar/glfw/3.3.8/lib >>>
+#include "MLX42/include/MLX42/MLX42.h"
+
---
+++ main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnieves- <fnieves-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 13:21:46 by fnieves-          #+#    #+#             */
+/*   Updated: 2022/09/03 13:21:47 by fnieves-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "MLX42/include/MLX42/MLX42.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <memory.h>
+#define WIDTH 500
+#define HEIGHT 256
+
+mlx_image_t	*g_img;
+
+void	hook(void *param)
+{
+	mlx_t	*mlx;
+
+	mlx = param;
+	if (mlx_is_key_down(param, MLX_KEY_ESCAPE))
+		mlx_close_window(param);
+	if (mlx_is_key_down(param, MLX_KEY_UP))
+		g_img->instances[0].y -= 5;
+	if (mlx_is_key_down(param, MLX_KEY_DOWN))
+		g_img->instances[0].y += 5;
+	if (mlx_is_key_down(param, MLX_KEY_LEFT))
+		g_img->instances[0].x -= 5;
+	if (mlx_is_key_down(param, MLX_KEY_RIGHT))
+		g_img->instances[0].x += 5;
+}
+
+int32_t	main(void)
+{
+	mlx_t	*mlx;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!mlx)
+		exit(EXIT_FAILURE);
+	g_img = mlx_new_image(mlx, 128, 128);
+	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
+	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
